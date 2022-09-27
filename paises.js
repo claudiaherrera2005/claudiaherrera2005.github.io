74344beaa7c874315cabf3dfa5d4bbb62762b555
claudiaherrera2005 <111001676+claudiaherrera2005@users.noreply.github.com>
2022-09-27 12:44:23 -0600

    Add files via upload

---
+++ paises.js
@@ -0,0 +1,27 @@
+function paises(){
+    var paises = parseFloat
+    (document.getElementById('pais').value);
+    switch(paises)
+    {
+        case 1:
+            document.getElementById('resultado').value = 'Guatemala';
+           
+            break;
+        case 2:
+            document.getElementById('resultado').value = 'Honduras';
+            break;
+        case 3:
+            document.getElementById('resultado').value = 'El salvador';
+            break;
+        case 4:
+            document.getElementById('resultado').value = 'Nicaragua';
+            break;
+        case 5:
+            document.getElementById('resultado').value = 'Costa rica';
+            break;
+        default:
+            document.write('No definido en la lista');
+            break;
+    }
+
+}
